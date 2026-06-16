44ad974bb5a116da8cb369bffce4a94e0ff66ea2
_kote <logkedr18@gmail.com>
2026-06-16 15:08:47 +0400

    fix(src): Удаление нетворкинга у серверного компонента

--- Content.Server/_ECHO/Battery/Components/APCBatteryDrinkerComponent.cs
+++ Content.Server/_ECHO/Battery/Components/APCBatteryDrinkerComponent.cs
@@ -6,13 +6,13 @@ namespace Content.Server._ECHO.Battery;
 /// <summary>
 /// Allows entity to recharge its battery from APC
 /// </summary>
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[RegisterComponent]
 public sealed partial class APCBatteryDrinkerComponent : Component
 {
-    [DataField, AutoNetworkedField]
+    [DataField]
     public float DrainSpeed = 1f;
 
-    [DataField, AutoNetworkedField]
+    [DataField]
     public float DrainAmount = 15f;
 
     [ViewVariables]
