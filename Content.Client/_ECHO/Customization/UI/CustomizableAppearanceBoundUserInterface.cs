9822181579342875705e2cfb9e1e6da8b122454e
_kote <logkedr18@gmail.com>
2026-06-26 01:16:22 +0400

    wip(src, ui): Окно мидраунд кастомизации

---
+++ Content.Client/_ECHO/Customization/UI/CustomizableAppearanceBoundUserInterface.cs
@@ -0,0 +1,46 @@
+using Content.Client.Humanoid;
+using Content.Shared._ECHO.Customization;
+using Content.Shared.MagicMirror;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._ECHO.Customization.UI;
+
+public sealed class CustomizableAppearanceBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables]
+    private CustomizableAppearanceWindow? _window;
+
+    private readonly MarkingsViewModel _markingsModel = new();
+
+    public CustomizableAppearanceBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = this.CreateWindow<CustomizableAppearanceWindow>();
+
+        _window.Title = EntMan.GetComponent<MetaDataComponent>(Owner).EntityName;
+
+        _window.MarkingsPicker.SetModel(_markingsModel);
+
+        _markingsModel.MarkingsChanged += (_, _) =>
+        {
+            SendMessage(new CustomizableAppearanceSelectMarkingMessage(_markingsModel.Markings));
+        };
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+
+        if (state is not CustomizableAppearanceBoundUserInterfaceState data)
+            return;
+
+        _markingsModel.OrganData = data.OrganMarkingData;
+        _markingsModel.OrganProfileData = data.OrganProfileData;
+        _markingsModel.Markings = data.AppliedMarkings;
+    }
+}
